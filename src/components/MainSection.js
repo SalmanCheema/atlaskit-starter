b6874ef624ab07d4b43b5d2f834aca4506f8f5ce
Salman Cheema <salman-cheema@hotmail.com>
2019-04-16 00:58:48 +0500

    Content update

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -6,7 +6,7 @@ export default () => (
       This application is bootstrapped using <a href="https://github.com/facebook/create-react-app">create-react-app</a> and <a href="https://atlaskit.atlassian.com/">Atlaskit</a>.
     </p>
     <p>
-      This gives a working navigation component you can start hacking on, see /src/components/StarterNavigation.js.
+      This gives an awesome working navigation component you can start hacking on, see /src/components/StarterNavigation.js.
     </p>
   </section>
 );
